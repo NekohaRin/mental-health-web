3245fe27b6eceee4d8c2520907c1544a69e6e5b0
NekohaRin <scinceninja9@gmail.com>
2025-11-26 14:57:07 +0700

    tambahan

--- public/index.php
+++ public/index.php
@@ -1,4 +1,86 @@
+<?php
+// include "config/session_protected.php";
+?>
+
 <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - MindEase</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="shortcut icon" href="assets/img/mindease.ico" type="image/x-icon">
+</head>
+
+<body class="bg-lime-50">
+    <nav class="flex flex-row max-w-full mx-auto justify-between items-center mt-3">
+        <h2 class="ml-9 text-3xl hover:text-lime-400 font-semibold">MindEase</h2>
+        <ul class="list-none flex flex-row gap-x-6 bg-lime-200 px-1.5 py-2 rounded-full">
+            <li><a href="#" class=" bg-lime-50 px-4 rounded-full py-1.5">Beranda</a></li>
+            <li><a href="pages/teskesehatan.php" class="hover:bg-lime-50 px-4 rounded-full py-1.5">Tes Kesehatan</a></li>
+            <li><a href="" class="hover:bg-lime-50 px-4 rounded-full py-1.5">Artikel & Tips</a></li>
+            <li><a href="" class="hover:bg-lime-50 px-4 rounded-full py-1.5">Daftar Konsultasi</a></li>
+            <li><a href="" class="hover:bg-lime-50 px-4 rounded-full py-1.5">Tentang Kami</a></li>
+        </ul>
+        <a href="pages/login_email.php" class="mr-9 text-base bg-lime-600 text-white py-3 rounded-full font-bold px-8 hover:bg-lime-400">Login</a>
+    </nav>
+
+    <!-- MAIN SECTION -->
+    <div class="container mx-auto flex flex-col items-center justify-center mt-4 text-center">
+        <div class="hero">
+            <img src="assets/img/beground1.png" alt="" class="rounded-xl">
+
+            <audio id="bg-audio" autoplay loop>
+                <source src="assets/audio/audio.mp3" type="audio/mpeg">
+            </audio>
+
+            <button id="audioToggle"
+                class="fixed bottom-4 left-4 p-2 px-3 bg-gray-800 text-white rounded-full hover:bg-gray-700 mt-3">
+                🔊
+            </button>
+        </div>
+
+        <div class="description mt-7 mb-5">
+            <h1 class="text-3xl mt-4 mb-3">About MindEase</h1>
+            <p class="mt-2">
+                MindEase adalah platform kesehatan mental yang dirancang untuk membantu pengguna memahami, memantau, dan meningkatkan kesejahteraan emosional mereka. Dengan tampilan yang sederhana, modern, dan ramah pengguna, MindEase menghadirkan berbagai fitur yang memudahkan siapa saja untuk mulai peduli pada kondisi mental mereka tanpa rasa canggung atau keraguan. Setiap bagian aplikasi dibuat agar mudah diakses, baik oleh pemula maupun pengguna yang sudah terbiasa dengan layanan digital. </p>
+            </p>
+
+            <p class="mt-5">
+                MindEase menyediakan berbagai tes kesehatan mental yang telah disesuaikan dengan standar psikologi populer, seperti tes tingkat stres, kecemasan, suasana hati, hingga keseimbangan hidup. Hasil tes akan ditampilkan secara jelas sehingga pengguna dapat memahami kondisi diri mereka dengan lebih baik. Platform ini juga dilengkapi artikel dan tips kesehatan mental yang kredibel untuk membantu pengguna belajar menangani tantangan emosional sehari‑hari. Selain itu, MindEase menawarkan fitur konsultasi yang memungkinkan pengguna mengatur jadwal atau mendaftarkan sesi dengan konselor atau psikolog yang tersedia.
+            </p>
+        </div>
+        <div class="cards mt-5 flex gap-5 mb-7 justify-between w-full">
+            <div class="w-xs h-30 bg-lime-400 border-2 rounded-2xl border-amber-200">Burnout</div>
+            <div class="w-xs h-30 bg-lime-400 border-2 rounded-2xl border-amber-200">Depresi</div>
+            <div class="w-xs h-30 bg-lime-400 border-2 rounded-2xl border-amber-200">Kecemasan</div>
+            <div class="w-xs h-30 bg-lime-400 border-2 rounded-2xl border-amber-200">Stress</div>
+        </div>
+    </div>
+
+    <script>
+        const audio = document.getElementById('bg-audio');
+        const btn = document.getElementById('audioToggle');
+
+        let muted = false;
+
+        btn.onclick = () => {
+            muted = !muted;
+            audio.muted = muted;
+
+            btn.textContent = muted ? "🔇" : "🔊";
+        };
+    </script>
+
+</body>
+
+</html>
+
+
+
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -90,4 +172,4 @@
     <br>
 </body>
 
-</html>
+</html> -->
